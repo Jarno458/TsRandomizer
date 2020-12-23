e28526cdf5d0313ec1db241590528429b570ba7c
Jarno Westhof <jwesthof@LIVE.NL>
2020-12-23 22:30:04 +0100

    Fixed verison numbering

--- TsRandomizer.ItemTracker/Properties/AssemblyInfo.cs
+++ TsRandomizer.ItemTracker/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("77705356-2595-478C-B6BF-3BA82A887C8B")]
-[assembly: AssemblyVersion("0.1.4.0")]
-[assembly: AssemblyFileVersion("0.1.4.0")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
 [assembly: InternalsVisibleTo("TsRandomizer.Tests")]
