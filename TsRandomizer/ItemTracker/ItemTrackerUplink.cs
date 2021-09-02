6e694efe5ddc267a13956991886d8a352a2ee251
Jarno Westhof <jarnowesthof@gmail.com>
2021-09-02 23:29:31 +0200

    Fixed item tracker

--- TsRandomizer/ItemTracker/ItemTrackerUplink.cs
+++ TsRandomizer/ItemTracker/ItemTrackerUplink.cs
@@ -7,22 +7,27 @@ namespace TsRandomizer.ItemTracker
 {
 	public static class ItemTrackerUplink
 	{
-        static readonly string StateFilePath = Path.GetTempPath() + "TsRandomizerItemTrackerState";
-        // NOTE FileOptions.DeleteOnClose is a nice idea but creates issues if a file handle still in use by an active process is unmapped.
-        // This can be done if only the game creates/deletes files and the tracker refreshes until it sees a file.
-        // That would also be a good first step toward supporting multiple simultaneous instances (game could append PID to filename, tracker would need selection UI)
-        static readonly FileStream FileStream = new FileStream(StateFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite | FileShare.Delete, 4096);
-        const int serializerOverheadSize = 500;
-        const int fileSize = sizeof(bool) * ItemTrackerState.NumberOfItems + serializerOverheadSize;
-		static readonly MemoryMappedFile MemoryMappedFile = GetMemoryMappedFile();
+		const int SerializerOverheadSize = 500;
+		const int FileSize = sizeof(bool) * ItemTrackerState.NumberOfItems + SerializerOverheadSize;
+
+		static readonly string StateFilePath = Path.GetTempPath() + "TsRandomizerItemTrackerState";
+
+        //static readonly FileStream FileStream = new FileStream(StateFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite | FileShare.Delete, FileSize);
+
+		//static readonly MemoryMappedFile MemoryMappedFile = GetMemoryMappedFile();
 		static ItemTrackerState lastSuccessfullRead;
 
 		public static void UpdateState(ItemTrackerState state)
 		{
 			var formatter = new BinaryFormatter();
 
-			using (var stream = GetMemoryMappedFileStream(MemoryMappedFileAccess.Write))
+			using (var fileStream = new FileStream(StateFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, FileSize))
+			using (var memmoryMappedFIle = MemoryMappedFile.CreateFromFile(fileStream, "TsRandomizerItemTrackerState", fileStream.Length, MemoryMappedFileAccess.ReadWrite, null, HandleInheritability.Inheritable, true))
+			using (var stream = memmoryMappedFIle.CreateViewStream(0, fileStream.Length, MemoryMappedFileAccess.Write))
 				formatter.Serialize(stream, state);
+
+			//using (var stream = GetMemoryMappedFileStream(MemoryMappedFileAccess.Write))
+			//formatter.Serialize(stream, state);
 		}
 
 		public static ItemTrackerState LoadState()
@@ -31,17 +36,19 @@ namespace TsRandomizer.ItemTracker
 
 			try
 			{
-				using (var stream = GetMemoryMappedFileStream(MemoryMappedFileAccess.Read))
+				using (var fileStream = new FileStream(StateFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, FileSize))
+				using (var memmoryMappedFIle = MemoryMappedFile.CreateFromFile(fileStream, "TsRandomizerItemTrackerState", fileStream.Length, MemoryMappedFileAccess.ReadWrite, null, HandleInheritability.Inheritable, true))
+				using (var stream = memmoryMappedFIle.CreateViewStream(0, fileStream.Length, MemoryMappedFileAccess.Read))
 					lastSuccessfullRead = (ItemTrackerState)formatter.Deserialize(stream);
 			}
-			catch (SerializationException)
+			catch
 			{
 			}
 
 			return lastSuccessfullRead;
 		}
 
-		static Stream GetMemoryMappedFileStream(MemoryMappedFileAccess access)
+		/*static Stream GetMemoryMappedFileStream(MemoryMappedFileAccess access)
 		{
 			return MemoryMappedFile.CreateViewStream(0, FileStream.Length, access);
 		}
@@ -50,15 +57,16 @@ namespace TsRandomizer.ItemTracker
 		{
             try
             {
-                FileStream.SetLength(fileSize);
-                MemoryMappedFile m = MemoryMappedFile.CreateFromFile(FileStream,"TsRandomizerItemTrackerState",FileStream.Length, MemoryMappedFileAccess.ReadWrite, null, HandleInheritability.Inheritable, true);
+                //FileStream.SetLength(FileSize);
+
+                var m = MemoryMappedFile.CreateFromFile(FileStream,"TsRandomizerItemTrackerState",FileStream.Length, MemoryMappedFileAccess.ReadWrite, null, HandleInheritability.Inheritable, true);
                 return m;
             }
             catch (System.Exception e)
             {
-                System.Console.WriteLine("ERROR opening MemoryMappedFile: " + e.ToString());
+                System.Console.WriteLine("ERROR opening MemoryMappedFile: " + e);
             }
             return null;
-		}
+		}*/
 	}
 }
