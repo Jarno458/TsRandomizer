78e564927ddaea57791c246661b3cdcf553ffeb4
Jarno Westhof <jarnowesthof@gmail.com>
2021-10-20 23:55:40 +0200

    Fixed message log to not keep message stuck on queue Message now fade when they are about to expire

--- TsRandomizer/Archipelago/Log.cs
+++ TsRandomizer/Archipelago/Log.cs
@@ -9,7 +9,12 @@ namespace TsRandomizer.Archipelago
 {
 	class Log : Overlay
 	{
-		const double FadeDelayInSeconds = 5;
+		const double FadeStartDelayInSeconds = 4;
+		const double FadeEndDelayInSeconds = 5;
+
+		static readonly TimeSpan FadeStart = TimeSpan.FromSeconds(FadeStartDelayInSeconds);
+		static readonly TimeSpan FadeEnd = TimeSpan.FromSeconds(FadeEndDelayInSeconds);
+		static readonly TimeSpan FadeTime = FadeEnd - FadeStart;
 
 		readonly GCM gcm;
 
@@ -34,7 +39,7 @@ namespace TsRandomizer.Archipelago
 
 		public override void Update(GameTime gameTime)
 		{
-			while (lines.TryPeek(out var message) && DateTime.UtcNow - message.TimeAdded > TimeSpan.FromSeconds(FadeDelayInSeconds))
+			while (lines.TryPeek(out var message) && message.OnScreenTime > FadeEnd)
 				lines.TryDequeue(out _);
 
 			CopyMessagesBetweenQueues(lines, pendingImportantLines);
@@ -44,7 +49,7 @@ namespace TsRandomizer.Archipelago
 		static void CopyMessagesBetweenQueues(
 			ConcurrentQueue<Message> destination, ConcurrentQueue<Message> source, int maxDestinationCount = 6)
 		{
-			while (destination.Count < maxDestinationCount && source.Count > 1)
+			while (destination.Count < maxDestinationCount && source.Count >= 1)
 			{
 				if (!source.TryDequeue(out var message))
 					return;
@@ -66,18 +71,22 @@ namespace TsRandomizer.Archipelago
 
 					var point = new Point(screenSize.X, (int)(screenSize.Height - lineHeight/2 - lineHeight*i));
 
-					DrawBackdrop(spriteBatch, point, screenSize, (int)lineHeight);
-					DrawMessage(spriteBatch, point, message);
+					double fade = (message.OnScreenTime > FadeStart)
+						? 1 - ((message.OnScreenTime - FadeStart).Ticks / (double)FadeTime.Ticks)
+						: 1;
+					
+					DrawBackdrop(spriteBatch, point, screenSize, (int)lineHeight, fade);
+					DrawMessage(spriteBatch, point, message, fade);
 
 					i++;
 				}
 			}
 		}
 
-		void DrawBackdrop(SpriteBatch spriteBatch, Point drawPoint, Rectangle screenSize, int lineHeight)
+		void DrawBackdrop(SpriteBatch spriteBatch, Point drawPoint, Rectangle screenSize, int lineHeight, double alpha)
 		{
 			var backdropColor = Color.Black;
-			backdropColor.A = 200;
+			backdropColor.A = (byte)(200 * alpha);
 
 			var origin = new Vector2(0,0);
 
@@ -86,7 +95,7 @@ namespace TsRandomizer.Archipelago
 			spriteBatch.Draw(gcm.TxBlankSquare, backdropArea, null, backdropColor, 0, origin, SpriteEffects.None, 1);
 		}
 
-		void DrawMessage(SpriteBatch spriteBatch, Point drawPoint, Message message)
+		void DrawMessage(SpriteBatch spriteBatch, Point drawPoint, Message message, double alpha)
 		{
 			var font = gcm.LatinFont;
 			var inGameZoom = (int)TimeSpinnerGame.Constants.InGameZoom;
@@ -95,8 +104,10 @@ namespace TsRandomizer.Archipelago
 			foreach (var part in message.Parts)
 			{
 				var position = new Vector2(drawPoint.X + partOffset, drawPoint.Y);
+				var color = part.Color;
+				color.A = (byte)(255 * alpha);
 
-				spriteBatch.DrawString(font, part.Text, position, part.Color, inGameZoom * 0.5f);
+				spriteBatch.DrawString(font, part.Text, position, color, inGameZoom * 0.5f);
 
 				partOffset += font.MeasureString(part.Text).X * inGameZoom * 0.5f;
 			}
@@ -105,11 +116,14 @@ namespace TsRandomizer.Archipelago
 
 	class Message
 	{
+		public TimeSpan OnScreenTime => DateTime.UtcNow - TimeAdded;
+
 		public DateTime TimeAdded { get; set; }
 		public Part[] Parts { get; }
 
 		public Message(Part[] parts)
 		{
+			TimeAdded = DateTime.MaxValue;
 			Parts = parts;
 		}
 	}
