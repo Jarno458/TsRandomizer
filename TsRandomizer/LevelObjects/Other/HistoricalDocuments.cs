76d8bcc522440e4499dea2bfcdd055e3519ca7b5
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2021-11-18 15:13:16 -0700

    Only override historical documents when flag is active

--- TsRandomizer/LevelObjects/Other/HistoricalDocuments.cs
+++ TsRandomizer/LevelObjects/Other/HistoricalDocuments.cs
@@ -7,10 +7,17 @@ namespace TsRandomizer.LevelObjects.Other
 	// ReSharper disable once UnusedMember.Global
 	class HistoricalDocuments: LevelObject
 	{
+		bool gyreArchivesEnabled = false;
 		public HistoricalDocuments(Mobile typedObject) : base(typedObject)
 		{
-			TimeSpinnerGame.Localizer.OverrideKey("q_ram_4_lun_29alt",
-				"It says, 'Redacted Temporal Research: Lord of Ravens'. Maybe I should ask the crow about this...");
+			if (gyreArchivesEnabled)
+				TimeSpinnerGame.Localizer.OverrideKey("q_ram_4_lun_29alt",
+					"It says, 'Redacted Temporal Research: Lord of Ravens'. Maybe I should ask the crow about this...");
 		}
-    }
+
+		protected override void Initialize(SeedOptions options)
+		{
+			gyreArchivesEnabled = options.GyreArchives;
+		}
+	}
 }
