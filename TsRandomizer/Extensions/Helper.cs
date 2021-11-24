58cfd3df92895d84707ddc80690b96ce211d3920
Colin Lenzen <32756996+TriumphantBass@users.noreply.github.com>
2021-11-23 20:39:05 -0700

    Extend helper to all Inventory Types

--- TsRandomizer/Extensions/Helper.cs
+++ TsRandomizer/Extensions/Helper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using Timespinner.GameAbstractions.Inventory;
 
@@ -11,6 +11,36 @@ namespace TsRandomizer.Extensions
 			return ((EInventoryOrbType[])Enum.GetValues(typeof(EInventoryOrbType)))
 				.Where(o => o != EInventoryOrbType.None && o != EInventoryOrbType.Monske)
 				.ToArray();
+		}
+
+		public static EInventoryJournalType[] GetAllJournals()
+		{
+			return ((EInventoryJournalType[])Enum.GetValues(typeof(EInventoryJournalType))).ToArray();
+		}
+
+		public static EInventoryEquipmentType[] GetAllEquipment()
+		{
+			return ((EInventoryEquipmentType[])Enum.GetValues(typeof(EInventoryEquipmentType))).ToArray();
+		}
+
+		public static EInventoryRelicType[] GetAllRelics()
+		{
+			return ((EInventoryRelicType[])Enum.GetValues(typeof(EInventoryRelicType))).ToArray();
+		}
+
+		public static EInventoryUseItemType[] GetAllUseItems()
+		{
+			return ((EInventoryUseItemType[])Enum.GetValues(typeof(EInventoryUseItemType))).ToArray();
+		}
+
+		public static EInventoryFamiliarType[] GetAllFamiliars()
+		{
+			return ((EInventoryFamiliarType[])Enum.GetValues(typeof(EInventoryFamiliarType))).ToArray();
+		}
+
+		public static EInventoryCategoryType[] GetAllInventoryCategories()
+		{
+			return ((EInventoryCategoryType[])Enum.GetValues(typeof(EInventoryCategoryType))).ToArray();
 		}
 	}
 }
