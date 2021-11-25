9f2a30bfff68c5e04ee897a7f6b0cd43ef6abbe4
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2021-11-25 15:09:01 -0700

    Revert changes from older branch

--- TsRandomizer/Extensions/Helper.cs
+++ TsRandomizer/Extensions/Helper.cs
@@ -12,35 +12,5 @@ namespace TsRandomizer.Extensions
 				.Where(o => o != EInventoryOrbType.None && o != EInventoryOrbType.Monske)
 				.ToArray();
 		}
-
-		public static EInventoryJournalType[] GetAllJournals()
-		{
-			return ((EInventoryJournalType[])Enum.GetValues(typeof(EInventoryJournalType))).ToArray();
-		}
-
-		public static EInventoryEquipmentType[] GetAllEquipment()
-		{
-			return ((EInventoryEquipmentType[])Enum.GetValues(typeof(EInventoryEquipmentType))).ToArray();
-		}
-
-		public static EInventoryRelicType[] GetAllRelics()
-		{
-			return ((EInventoryRelicType[])Enum.GetValues(typeof(EInventoryRelicType))).ToArray();
-		}
-
-		public static EInventoryUseItemType[] GetAllUseItems()
-		{
-			return ((EInventoryUseItemType[])Enum.GetValues(typeof(EInventoryUseItemType))).ToArray();
-		}
-
-		public static EInventoryFamiliarType[] GetAllFamiliars()
-		{
-			return ((EInventoryFamiliarType[])Enum.GetValues(typeof(EInventoryFamiliarType))).ToArray();
-		}
-
-		public static EInventoryCategoryType[] GetAllInventoryCategories()
-		{
-			return ((EInventoryCategoryType[])Enum.GetValues(typeof(EInventoryCategoryType))).ToArray();
-		}
 	}
 }
