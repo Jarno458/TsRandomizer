f32d7ae5ac3e141435eda60ec5362b22590e861a
weffjebster <weffjebster@gmail.com>
2021-12-12 20:55:56 -0500

    wip

---
+++ TsRandomizer/Screens/Settings/GameSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TsRandomizer.Screens.Settings
+{
+	abstract class GameSetting
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public dynamic CurrentValue { get; set; }
+		public dynamic DefaultValue { get; set; }
+
+		void Reset()
+		{
+			CurrentValue = DefaultValue;
+		}
+		
+	}
+}
---
+++ TsRandomizer/Screens/Settings/GameSettingsCollection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace TsRandomizer.Screens.Settings
+{
+	class GameSettingsCollection
+	{
+		private const string path = "/settings/";
+		[JsonIgnore()]
+		public string PresetName { get; set; }
+		public bool StartWithMeyef { get; set; }
+		public bool DamageRando { get; set; }
+		public string PlayerName { get; set; }
+
+		public GameSettingsCollection()
+		{
+			var settingsFiles = Directory.GetFiles(path).ToList().Where(f => f.EndsWith(".json"));
+			if(settingsFiles.Count() > 0)
+			{
+				var defaultFile = settingsFiles.First();
+				GameSettingsCollection settings = LoadSettingsFromFile(defaultFile);
+				PresetName = defaultFile.Substring(0, defaultFile.Length - 6);
+				StartWithMeyef = settings.StartWithMeyef;
+				DamageRando = settings.DamageRando;
+				PlayerName = settings.PlayerName;
+			}
+			
+		}
+
+		private GameSettingsCollection LoadSettingsFromFile(string fileName)
+		{
+			try
+			{
+				if (File.Exists(fileName))
+				{
+					string settingsString = File.ReadAllText(fileName);
+					GameSettingsCollection settings = JsonConvert.DeserializeObject<GameSettingsCollection>(settingsString);
+					return settings;
+				}
+				Console.WriteLine("Settings file not found: " + fileName);
+				return new GameSettingsCollection();
+			}
+			catch
+			{
+				Console.WriteLine("Error loading settings from " + fileName);
+				return new GameSettingsCollection();
+			}
+			
+		}
+
+		public GameSettingsCollection LoadSettings(string presetName)
+		{
+			try
+			{
+				var settingsFromFile = LoadSettingsFromFile($"{path}{presetName}.json");
+				settingsFromFile.PresetName = presetName;
+				return settingsFromFile;
+			}
+			catch
+			{
+				Console.WriteLine("Error loading settings for " + presetName);
+				return new GameSettingsCollection();
+			}
+			
+			
+		}
+
+		public bool WriteSettings(string presetName, GameSettingsCollection settings)
+		{
+			try
+			{
+				string jsonSettings = JsonConvert.SerializeObject(settings);
+				File.WriteAllText($"{path}{presetName}.json", jsonSettings);
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+			
+		}
+	}
+}
