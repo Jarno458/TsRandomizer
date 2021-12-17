bda51305805f02e9b1d3f40cec19a147196957ea
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2021-12-17 11:37:25 -0700

    More collections work

--- TsRandomizer/Screens/SeedSettings/SeedSettingsScreen.cs
+++ TsRandomizer/Screens/SeedSettings/SeedSettingsScreen.cs
@@ -9,6 +9,8 @@ using TsRandomizer.Randomisation;
 using TsRandomizer.Screens.Menu;
 using TsRandomizer.Screens.SeedSelection;
 
+using System.Collections;
+
 
 namespace TsRandomizer.Screens.SeedSettings
 {
@@ -37,6 +39,23 @@ namespace TsRandomizer.Screens.SeedSettings
 			var menuEntryList = new object[0].ToList(MenuEntryType);
 			var menuCollectionList = new object[0].ToList(MenuEntryCollectionType);
 
+			/*
+			var menus = ((IList)((object)Dynamic._primaryMenuCollection).AsDynamic()._entries)
+				.Cast<object>()
+				.ToList(MenuEntryType);
+			var placeHolderMenu = menus[4];
+
+			placeHolderMenu.AsDynamic().Text = "Stats";
+			menuEntryList.Add(placeHolderMenu);
+			placeHolderMenu.AsDynamic().Text = "Enemies";
+			menuEntryList.Add(placeHolderMenu);
+			placeHolderMenu.AsDynamic().Text = "Shops";
+			menuEntryList.Add(placeHolderMenu);
+			placeHolderMenu.AsDynamic().Text = "Sprites";
+			menuEntryList.Add(placeHolderMenu);
+			placeHolderMenu.AsDynamic().Text = "Other";
+			menuEntryList.Add(placeHolderMenu);*/
+
 			var scalingMenu = MenuEntry.Create("Stats", () => { });
 			scalingMenu.AsDynamic().Description = "Settings related to stat scaling.";
 			scalingMenu.AsDynamic().IsCenterAligned = false;
@@ -45,7 +64,11 @@ namespace TsRandomizer.Screens.SeedSettings
 			enemyMenu.Description = "Settings related to enemy placement and drops.";
 			enemyMenu.IsCenterAligned = false;
 
-			var spriteMenu = MenuEntry.Create("Sprites", () => { });
+			var shopMenu = MenuEntry.Create("Shops", () => { });
+			shopMenu.Description = "Settings related to shop inventory.";
+			shopMenu.IsCenterAligned = false;
+
+			var spriteMenu = MenuEntry.Create("Sprites", () => { }, false);
 			spriteMenu.Description = "Settings related to sprite replacement.";
 			spriteMenu.IsCenterAligned = false;
 
@@ -55,13 +78,28 @@ namespace TsRandomizer.Screens.SeedSettings
 
 			menuEntryList.Add(scalingMenu.AsTimeSpinnerMenuEntry());
 			menuEntryList.Add(enemyMenu.AsTimeSpinnerMenuEntry());
+			menuEntryList.Add(shopMenu.AsTimeSpinnerMenuEntry());
 			menuEntryList.Add(spriteMenu.AsTimeSpinnerMenuEntry());
 			menuEntryList.Add(otherMenu.AsTimeSpinnerMenuEntry());
 
 			((object)Dynamic._primaryMenuCollection).AsDynamic()._entries = menuEntryList;
-			// ((object)Dynamic._selectedMenuCollection).AsDynamic()._entries = menuEntryList;
-			
+
+			var collections = ((IList)Dynamic._subMenuCollections)
+				.Cast<object>()
+				.ToList(MenuEntryCollectionType);
+			var bestiary = collections[4];
+
+			menuCollectionList.Add(bestiary);
+			menuCollectionList.Add(bestiary);
+			menuCollectionList.Add(bestiary);
+			menuCollectionList.Add(bestiary);
+			menuCollectionList.Add(bestiary);
 			Dynamic._subMenuCollections = menuCollectionList;
+		}
+
+		public SeedSettingsScreen(ScreenManager screenManager, GameScreen passwordMenuScreen) : base(screenManager, passwordMenuScreen)
+		{
+			seedSelectionScreen = screenManager.FirstOrDefault<SeedSelectionMenuScreen>();
 		}
 
 		public static GameScreen Create(ScreenManager screenManager, SeedOptionsCollection options)
