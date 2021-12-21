63c0b9305aabd508547cd5ac7f3cff02989c00a5
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2021-12-20 17:21:41 -0700

    Remove Damage Rando flag after revert

--- TsRandomizer/SeedOptions.cs
+++ TsRandomizer/SeedOptions.cs
@@ -25,7 +25,6 @@ namespace TsRandomizer
 		public bool GyreArchives => (Flags & 1 << 10) > 0;
 		public bool Cantoran => (Flags & 1 << 11) > 0;
 		public bool LoreChecks => (Flags & 1 << 12) > 0;
-		public bool DamageRando => (Flags & 1 << 13) > 0;
 
 		//Non visable flags
 		public bool Archipelago => (Flags & 1 << 16) > 0;
@@ -54,8 +53,7 @@ namespace TsRandomizer
 				{"StinkyMaw", 1U << 9},
 				{"GyreArchives", 1U << 10},
 				{"Cantoran", 1U << 11},
-		{"LoreChecks", 1U << 12},
-				{"DamageRando", 1U << 13 },
+				{"LoreChecks", 1U << 12},
 				{"DeathLink", 1U << 17}
 			};
 
