5b225e1c604f31bb0a9f62a4e22c456062cc7714
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2022-05-05 23:14:34 -0700

    Add initial boss data

--- TsRandomizer/Randomisation/BestiaryManager.cs
+++ TsRandomizer/Randomisation/BestiaryManager.cs
@@ -7,8 +7,115 @@ using TsRandomizer.Settings;
 
 namespace TsRandomizer.Randomisation
 {
+	struct BossAttributes
+	{
+		public int Index;
+		public string VisibleName;
+		public RoomItemKey RoomKey;
+		public int HP;
+		public int XP;
+		public int TouchDamage;
+	}
+
 	static class BestiaryManager
 	{
+		private static BossAttributes GetBossAttributes(Level level, int bossId)
+		{
+			switch (bossId)
+			{
+				case 73:
+					return new BossAttributes
+					{
+						Index = bossId,
+						VisibleName = "Emperor Nuvius",
+						RoomKey = new RoomItemKey(12, 20),
+						HP = 3500,
+						XP = 666,
+						TouchDamage = 80
+					};
+				case 74:
+					return new BossAttributes
+					{
+						Index = bossId,
+						VisibleName = "Emperor Vol Terrilis",
+						RoomKey = new RoomItemKey(13, 1),
+						HP = 4000,
+						XP = 777,
+						TouchDamage = 85
+					};
+				case 75:
+					return new BossAttributes
+					{
+						Index = bossId,
+						VisibleName = "Prince Nuvius",
+						RoomKey = new RoomItemKey(13, 0),
+						HP = 2500,
+						XP = 350,
+						TouchDamage = 70
+					};
+				case 76:
+					return new BossAttributes
+					{
+						Index = bossId,
+						VisibleName = "Xarion",
+						RoomKey = new RoomItemKey(9, 7),
+						HP = 3500,
+						XP = 550,
+						TouchDamage = 75
+					};
+				case 77:
+					return new BossAttributes
+					{
+						Index = bossId,
+						VisibleName = "Ravenlord",
+						RoomKey = new RoomItemKey(14, 4),
+						HP = 5000,
+						XP = 680,
+						TouchDamage = 95
+					};
+				case 78:
+					return new BossAttributes
+					{
+						Index = bossId,
+						VisibleName = "Ifrit",
+						RoomKey = new RoomItemKey(14, 5),
+						HP = 5000,
+						XP = 700,
+						TouchDamage = 95
+					};
+				case 79: 
+					return new BossAttributes
+					{
+						Index = bossId,
+						VisibleName = "Sandman",
+						RoomKey = new RoomItemKey(7, 5),
+						HP = 5000,
+						XP = 800,
+						TouchDamage = 90
+					};
+				case 80:
+					return new BossAttributes
+					{
+						Index = bossId,
+						VisibleName = "Nightmare",
+						RoomKey = new RoomItemKey(12, 26),
+						HP = 6666,
+						XP = 0,
+						TouchDamage = 111
+					};
+				default: 
+					return new BossAttributes
+					{ 
+						Index = 10,
+						VisibleName = "Baby Cheveur",
+						RoomKey = new RoomItemKey(7, 5),
+						HP = 50,
+						XP = 9,
+						TouchDamage = 25
+					};
+			}
+		}
+
 		public static void UpdateBestiary(Level level, SeedOptions seedOptions, SettingCollection gameSettings)
 		{
 			TimeSpinnerGame.Localizer.OverrideKey("inv_use_PlaceHolderItem1", "Nothing");
@@ -22,6 +129,7 @@ namespace TsRandomizer.Randomisation
 				{
 					level.GameSave.SetValue(string.Format(bestiaryEntry.Key.Replace("Enemy_", "KILL_")), 1);
 				}
+				bestiaryEntry.VisibleDescription = bestiaryEntry.Key + " ~ " + bestiaryEntry.TouchDamage + "~" + bestiaryEntry.EnemyType;
 
 				foreach (var loot in bestiaryEntry.LootTable)
 				{
