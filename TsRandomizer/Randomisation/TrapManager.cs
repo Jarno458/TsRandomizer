7fe9d68f19f7c29e1e9d7dc744fdd7b05d0f2662
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2023-01-23 16:07:43 -0700

    Implement sparrow trap

--- TsRandomizer/Randomisation/TrapManager.cs
+++ TsRandomizer/Randomisation/TrapManager.cs
@@ -26,18 +26,23 @@ namespace TsRandomizer.Randomisation
 	{
 		public static void SparrowTrap(Level level)
 		{
-			var enemyTile = new ObjectTileSpecification
-			{
-				Category = EObjectTileCategory.Enemy,
-				Layer = ETileLayerType.Objects,
-				ObjectID = (int)EEnemyTileType.CastleArcher
-			};
+			ObjectTileSpecification enemyTile = new ObjectTileSpecification();
+			enemyTile.Category = EObjectTileCategory.Enemy;
+			enemyTile.Layer = ETileLayerType.Objects;
+			enemyTile.ObjectID = (int)EEnemyTileType.FlyingCheveux;
+
+			var lunaisPos = level.MainHero.LastPosition;
 			var sprite = level.GCM.SpGyreMeteorSparrow;
-			var enemyType = TimeSpinnerType.Get("Timespinner.GameObjects.Enemies.Monster.GyreMeteorSparrow");
-			var lunaisPos = level.MainHero.Position;
-			var position = new Point(lunaisPos.X + 200, lunaisPos.Y + 200);
-			var sparrow = enemyType.CreateInstance(false, position, level, sprite, enemyTile);
-			level.AsDynamic().RequestAddObject(sparrow);
+			var enemyType = TimeSpinnerType.Get("Timespinner.GameObjects.Enemies.GyreMeteorSparrow");
+			var enemy = enemyType.CreateInstance(false, new Point(lunaisPos.X + 100, lunaisPos.Y - 50), level, sprite, -1, enemyTile);
+			enemy.AsDynamic()._isAggroed = true;
+			level.AsDynamic().RequestAddObject(enemy);
+
+
+			enemy = enemyType.CreateInstance(false, new Point(lunaisPos.X - 100, lunaisPos.Y - 50), level, sprite, -1, enemyTile);
+			enemy.AsDynamic()._isAggroed = true;
+			level.AsDynamic().RequestAddObject(enemy);
+			level.JukeBox.PlayCue(Timespinner.GameAbstractions.ESFX.CsPrologueTableFlip);
 		}
 	}
 }
