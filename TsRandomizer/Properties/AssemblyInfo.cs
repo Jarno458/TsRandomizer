280cf7e74cbff869a3f38b7ba4ccba0bce2690b7
Jarno Westhof <jarnowesthof@gmail.com>
2024-06-22 21:05:44 +0200

    Updated things for 4.8

--- TsRandomizer/Properties/AssemblyInfo.cs
+++ TsRandomizer/Properties/AssemblyInfo.cs
@@ -12,8 +12,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("694e46c5-fd46-4cc8-8b71-d381e154ccf7")]
-[assembly: AssemblyVersion("1.29.1")]
-[assembly: AssemblyFileVersion("1.29.1")]
+[assembly: AssemblyVersion("1.29.4")]
+[assembly: AssemblyFileVersion("1.29.4")]
 
 [assembly: InternalsVisibleTo("TsRandomizer.Tests")]
 [assembly: InternalsVisibleTo("TsRandomizerSeedGeneratah")]
