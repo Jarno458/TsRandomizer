e302b9f079e60165aa91c4cdc32b62427494ca7a
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2024-11-04 17:31:37 -0700

    Experiment XIII

---
+++ TsRandomizer/LevelObjects/Monsters/LabExperiment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Archipelago.MultiClient.Net.Enums;
+using Microsoft.Xna.Framework;
+using Timespinner.Core.Specifications;
+using Timespinner.GameAbstractions;
+using Timespinner.GameAbstractions.Gameplay;
+using Timespinner.GameAbstractions.HUD;
+using Timespinner.GameObjects.BaseClasses;
+using Timespinner.GameObjects.Events.Cutscene;
+using TsRandomizer.Archipelago;
+using TsRandomizer.Extensions;
+using TsRandomizer.IntermediateObjects;
+using TsRandomizer.Randomisation;
+using TsRandomizer.Screens;
+using TsRandomizer.Settings;
+
+namespace TsRandomizer.LevelObjects.Monsters
+{
+	[TimeSpinnerType("Timespinner.GameObjects.Enemies.LabAdult")]
+	[TimeSpinnerType("Timespinner.GameObjects.Enemies.LabChild")]
+	// ReSharper disable once UnusedMember.Global
+	class LabExperiment: LevelObject<Monster>
+	{
+
+		protected override void Initialize(Seed seed, SettingCollection settings)
+		{
+			/*Level.ToggleExits(false);
+			Level.OpenAllBossDoors(-1f);
+			Level.LockAllBossDoors(0.5f);
+
+			Level.JukeBox.StopSong();
+			Level.JukeBox.PlaySong(EBGM.Library);*/
+		}
+
+		public LabExperiment(Monster typedObject, GameplayScreen gameplayScreen) : base(typedObject, gameplayScreen)
+		{
+			typedObject.InitializeMob();
+			// typedObject.SilentKill();
+		}
+
+
+
+		protected override void OnUpdate()
+		{
+		}
+	}
+}
---
+++ TsRandomizer/RoomTriggers/Triggers/LabExperimentRooms.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Timespinner.Core.Specifications;
+using TsRandomizer.Extensions;
+using TsRandomizer.IntermediateObjects;
+
+
+namespace TsRandomizer.RoomTriggers.Triggers
+{
+	[RoomTriggerTrigger(11, 1)] // Experiment 13
+	// [RoomTriggerTrigger(11, 35)] // Genza
+	// [RoomTriggerTrigger(11, 37)] // Research Wing
+	// 11, 39 also has a laser but is covered as Dynamo Works
+	class LabExperimentRooms : RoomTrigger
+	{
+		public override void OnRoomLoad(RoomState roomState)
+		{
+			SpawnExperiment(roomState, new Point(600, 176), true);
+		}
+
+		protected static void SpawnExperiment(RoomState state, Point point, bool isAdult)
+		{
+			var level = state.Level;
+			var enemyTile = new ObjectTileSpecification
+			{
+				Category = EObjectTileCategory.Enemy,
+				Layer = ETileLayerType.Objects,
+				ObjectID = isAdult ? (int)EEnemyTileType.LabAdult : (int)EEnemyTileType.LabChild,
+				Argument = 0,
+				IsFlippedHorizontally = false
+			};
+			var enemyType = isAdult ? TimeSpinnerType.Get("Timespinner.GameObjects.Enemies.LabAdult") : TimeSpinnerType.Get("Timespinner.GameObjects.Enemies.LabChild");
+			var sprite = isAdult ? level.GCM.SpLabAdult : level.GCM.SpLabChild;
+			var enemy = enemyType.CreateInstance(false, point, level, sprite, -1, enemyTile);
+			level.AsDynamic().RequestAddObject(enemy);
+		}
+	}
+}
