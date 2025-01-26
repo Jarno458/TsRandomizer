31871cc8c6acba1c679c34be4847ed92bb93afcf
sgrunt <sgrunt1987@gmail.com>
2025-01-26 14:58:40 -0700

    Allow AP to enable throw traps in slot data

--- TsRandomizer/Settings/GameSettingsLoader.cs
+++ TsRandomizer/Settings/GameSettingsLoader.cs
@@ -347,6 +347,7 @@ namespace TsRandomizer.Settings
 				settings.ChaosTrap.Value = traps.Contains("Chaos Trap");
 				settings.NeurotoxinTrap.Value = traps.Contains("Neurotoxin Trap");
 				settings.BeeTrap.Value = traps.Contains("Bee Trap");
+				settings.ThrowStunTrap.Value = traps.Contains("Throw Stun Trap");
 			}
 
 			if (slotData.TryGetValue("EnableMapFromStart", out var enableMapFromStart))
