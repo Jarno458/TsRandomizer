ac2354287403c87caad06fb4529d30033220d900
Jarno Westhof <jarnowesthof@gmail.com>
2025-01-29 17:52:23 +0100

    Updated item tracker version to v1.12

--- TsRandomizer.ItemTracker/Properties/AssemblyInfo.cs
+++ TsRandomizer.ItemTracker/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("77705356-2595-478C-B6BF-3BA82A887C8B")]
-[assembly: AssemblyVersion("1.11.0")]
-[assembly: AssemblyFileVersion("1.11.0")]
+[assembly: AssemblyVersion("1.12.0")]
+[assembly: AssemblyFileVersion("1.12.0")]
 [assembly: InternalsVisibleTo("TsRandomizer.Tests")]
