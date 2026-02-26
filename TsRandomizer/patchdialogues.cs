6a1b2ace7e79bc4faa438f53c9d2ca236413c83c
waze3174 <31178121+waze3174@users.noreply.github.com>
2026-02-25 19:48:34 -0500

    add patchdialogues and patchcutscenes.cs

---
+++ TsRandomizer/patchdialogues.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace TsRandomizer
+{
+	[HarmonyPatch]
+	class DialogueAutoSkipPatch
+	{
+		static MethodBase TargetMethod()
+		{
+			return AccessTools.Method(
+				AccessTools.TypeByName("Timespinner.GameAbstractions.HUD.DialogueBox"),
+				"Update");
+		}
+
+		static void Prefix(object __instance)
+		{
+			var type = AccessTools.TypeByName("Timespinner.GameAbstractions.HUD.DialogueBox");
+			AccessTools.Field(type, "_isAutoplay").SetValue(__instance, true);
+			AccessTools.Field(type, "_autoplayNextLineTimer").SetValue(__instance, 999f);
+			AccessTools.Field(type, "_isReadyForNextLine").SetValue(__instance, true);
+		}
+	}
+}
